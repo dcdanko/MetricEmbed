5390a347ac1094c8ef61fd70c2ed813655109a2e
tristanhull <YOUR EMAIL ADDRESS>
2016-11-14 14:58:00 -0500

    embedding parser

---
+++ parse.py
@@ -0,0 +1,25 @@
+import sys, math, numpy, operator
+
+embedding = open(sys.argv[1])
+
+word_vecs = {}
+total_words = 0
+
+for line in embedding: # Reads the lines of the input file, skipping the first line
+	vector = line.split()
+	total_words += 1
+	word_vecs[vector[0]] = map(lambda x: float(x), vector[1:])
+
+def l2norm(vector):
+	return numpy.sqrt(numpy.sum(map(lambda x : x * x, vector)))
+
+word_lengths = dict(map(lambda (k,v): (k, l2norm(v)), word_vecs.iteritems()))
+
+def print_sorted(pair_dict):
+	sorted_lengths = sorted(word_lengths.items(), key = operator.itemgetter(1))
+	print("\n".join(map(str, sorted_lengths[0:20])))
+        print("\n")
+	print("\n".join(map(str, sorted_lengths[-20:-1])))
+
+print_sorted(word_lengths)
+
