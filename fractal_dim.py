c98e8dbc262e1f1c91be61013c4f082c83cf002b
tristanhull <YOUR EMAIL ADDRESS>
2016-11-26 10:25:13 -0500

    more fractal di changes

--- fractal_dim.py
+++ fractal_dim.py
@@ -44,11 +44,13 @@ def buildArgs():
 import sys
 import pandas as pd
 import numpy as np
+import numpy.matlib
 import scipy.spatial.distance
 import subprocess as sp
 import parse
 import simpleMeasures as sm
 import math
+import matplotlib.pyplot as plt
 
 def findPointClouds(embeddings,nSamples,radii,metric):
     samples = embeddings.sample(n=nSamples,axis=1)
@@ -60,20 +62,33 @@ def findPointClouds(embeddings,nSamples,radii,metric):
 
     return nPoints
 
-def findPointCloud(embedding,radius):
+def findPointCloud(embedding,radius,center):
     """
     Computes the size of the point cloud of radius radius about the origin.
     """
-    l2norms = np.sqrt(np.square(embedding).sum(axis=0))
+    repCenter = np.matlib.repmat(center,embedding.shape[1],1).T
+    l2norms = np.sqrt(np.square(embedding-repCenter).sum(axis=0))
     return sum(map(lambda x : 1 if x < radius else 0,l2norms))
 
 def getMultiplicativeGrowthRate(cloudSizes):
     """
     Computes the average multiplicative growth rate (over indices)
     """
-    sortedCloudSizes = filter(lambda x : x > 0.5, cloudSizes)
-    ratios = np.divide(sortedCloudSizes[1:],sortedCloudSizes[:-1])
-    return sum(ratios) / (len(sortedCloudSizes) - 1)
+    #sortedCloudSizes = filter(lambda x : x > 0.5, cloudSizes)
+    sortedRadii = filter(lambda x : cloudSizes[x] > 0.5, sorted(cloudSizes))
+    sortedCloudSizes = filter(lambda x : x > 0.5, map(lambda x : float(cloudSizes[x]), sortedRadii))
+    print(sortedCloudSizes)
+    countRatios = np.divide(sortedCloudSizes[1:],sortedCloudSizes[:-1])
+    radiusRatios = np.divide(sortedRadii[1:],sortedRadii[:-1])
+    logs = np.divide(np.log(countRatios),np.log(radiusRatios))
+    
+    # PLOTTING HERE
+    plt.plot(logs)
+    plt.show()
+    # END PLOTTING
+
+    #return sum(countRatios) / (len(sortedCloudSizes) - 1)
+    return sum(logs) / (len(sortedRadii) - 1)
 
 def fractalDimension(embedding,initRad,radFactor,radCount):
     """
@@ -81,19 +96,24 @@ def fractalDimension(embedding,initRad,radFactor,radCount):
     Scanns radii initRad*radFactor^0 -> initRad*radFactor^(radCount-1)
     """
     avgDist = sm.averagePairwiseDistance(embedding)
-    cloudSizes = np.zeros(radCount)
+    center = .1 * np.random.randn(embedding.shape[0])
+    #cloudSizes = np.zeros(radCount)
+    cloudSizes = {}
     radius = initRad * avgDist
     for i in range(0,radCount):
-        cloudSizes[i] = findPointCloud(embedding,radius)
-        radius = radius * radFactor
+        #cloudSizes[i] = findPointCloud(embedding,radius,center)
+        cloudSizes[radius] = findPointCloud(embedding,radius,center)
+        #radius = radius * radFactor
+        radius = radius + (radFactor * avgDist)
     growthRate = getMultiplicativeGrowthRate(cloudSizes)
-    return math.log(growthRate,radFactor)
+    #return math.log(growthRate,radFactor)
+    return growthRate
 
 def main():
     args = buildArgs()
     #embeddings = pd.DataFrame.from_csv(args.embeddingf,sep=args.sep,header=None)
     embeddings = pd.DataFrame.from_dict(parse.parse(args.embeddingf))
-    dim = fractalDimension(embeddings,0.01,1.2,10)
+    dim = fractalDimension(embeddings,0.,.00025,100)
     print('{}: {}'.format('Fractal Dimension',dim))
     #cloudSizes = findPointClouds(embeddings,args.samples,args.radius,'euclidean')
     #for r,s in cloudSizes.items():
